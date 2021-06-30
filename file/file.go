1ee1536a7398d3bd6c8e1c35ae6665ee6346abd2
xerardoo <gerardoo_2413@hotmail.com>
2021-06-30 04:15:45 -0700

    file methods implemented

--- file/file.go
+++ file/file.go
@@ -1,27 +1,52 @@
 package file
 
-import "errors"
+import (
+	"errors"
+	"io/ioutil"
+	"net/http"
+)
 
-var MaxFileSize = 50
+var MaxFileSize float64 = 5
 var ErrMaxFileSize = errors.New("file too large")
+var ErrLimitSizeNoValid = errors.New("file limit size no valid")
 
 type File struct {
 	Data []byte
 	Url  string
-	Size int
+	Size float64
 }
 
-func (f *File) Fetch(url string) (data []byte, err error) {
-	if MaxFileSize > 0 {
+func (f *File) Fetch(url string) ([]byte, error) {
+	f.Url = url
+	resp, err := http.Get(f.Url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	f.Data, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	f.Size = float64(len(f.Data)) / 1024 / 1024
+	if f.Size > MaxFileSize {
 		return nil, ErrMaxFileSize
+	}
+	return f.Data, nil
+}
+
+func (f *File) Write(path string) (err error) {
+	err = ioutil.WriteFile(path, f.Data, 775)
+	if err != nil {
+		return
 	}
 	return
 }
 
-func (f *File) Write(path string) (err error) {
-	return
-}
-
-func (f *File) SetLimitSize(mb int) (err error) {
+func SetLimitSize(mb float64) (err error) {
+	if mb <= 0 {
+		return ErrLimitSizeNoValid
+	}
+	MaxFileSize = mb
 	return
 }
--- file/file_test.go
+++ file/file_test.go
@@ -4,6 +4,7 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/xerardoo/hash-example/file"
+	"os"
 	"testing"
 )
 
@@ -15,20 +16,30 @@ func TestFile(t *testing.T) {
 var _ = Describe("File testing", func() {
 	input1 := "https://www.learningcontainer.com/wp-content/uploads/2020/04/sample-text-file.txt"
 	var newFile file.File
-	var limitSize = 100
+	var limitSize float64 = 100
+	var path = ".././my-file.txt"
+	os.Remove(path)
 
-	_, err := newFile.Fetch(input1)
+	doc, err := newFile.Fetch(input1)
 	It("file be fetched", func() {
 		Expect(err).NotTo(HaveOccurred())
+		Expect(len(doc)).Should(BeNumerically(">", 0))
 	})
 
-	err2 := newFile.Write("./my-file.txt")
+	err4 := file.SetLimitSize(limitSize)
+	It("file size be limited", func() {
+		Expect(err4).NotTo(HaveOccurred())
+		Expect(file.MaxFileSize).Should(Equal(limitSize))
+	})
+
+	doc, err = newFile.Fetch(input1)
+	It("file be fetched", func() {
+		Expect(err).NotTo(HaveOccurred())
+		Expect(len(doc)).Should(BeNumerically(">", 0))
+	})
+
+	err2 := newFile.Write(path)
 	It("file be writed", func() {
 		Expect(err2).NotTo(HaveOccurred())
-	})
-
-	err3 := newFile.SetLimitSize(limitSize)
-	It("file size be limited", func() {
-		Expect(err3).NotTo(HaveOccurred())
 	})
 })
