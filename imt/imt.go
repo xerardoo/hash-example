6f2dc6080bc1942746b0381acdfa84aaeb18c3ce
xerardoo <gerardoo_2413@hotmail.com>
2021-06-30 02:17:26 -0700

    generate imt hash function implemented

--- imt/imt.go
+++ imt/imt.go
@@ -1,8 +1,24 @@
 package imt
 
-import "fmt"
+var coefficients = [8]int{2, 3, 5, 7, 11, 13, 17, 19}
 
-func Generate(data []byte) (hash []byte, err error) {
-	fmt.Println("test", data)
-	return
+// Function for generate IMT Hash
+func Generate(data []byte) ([]byte, error) {
+	var tempHash = make([]int, len(data)*8)
+	var hash []byte
+
+	for _, ib := range data {
+		for i, coefficient := range coefficients {
+			var temp int
+			if i > 0 {
+				temp = i - 1
+			}
+			tempHash[i] = ((tempHash[temp] + int(ib)) * coefficient) % 255
+		}
+	}
+	// []int to []byte Conversion - Is not elegant, but works fine
+	for _, h := range tempHash {
+		hash = append(hash, byte(h))
+	}
+	return hash, nil
 }
