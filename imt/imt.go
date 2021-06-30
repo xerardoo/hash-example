f19d2a422f7551ed32066ec194d146e5ad3a537a
xerardoo <gerardoo_2413@hotmail.com>
2021-06-30 02:37:27 -0700

    error caching on generate imt function

--- imt/imt.go
+++ imt/imt.go
@@ -1,14 +1,20 @@
 package imt
 
-var coefficients = [8]int{2, 3, 5, 7, 11, 13, 17, 19}
+import "errors"
+
+var ErrEmptySlice = errors.New("empty slice")
+var Coefficients = [8]int{2, 3, 5, 7, 11, 13, 17, 19}
 
 // Function for generate IMT Hash
 func Generate(data []byte) ([]byte, error) {
+	if len(data) == 0 {
+		return nil, ErrEmptySlice
+	}
 	var tempHash = make([]int, len(data)*8)
 	var hash []byte
 
 	for _, ib := range data {
-		for i, coefficient := range coefficients {
+		for i, coefficient := range Coefficients {
 			var temp int
 			if i > 0 {
 				temp = i - 1
--- imt/imt_test.go
+++ imt/imt_test.go
@@ -1,6 +1,7 @@
 package imt_test
 
 import (
+	"encoding/hex"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/xerardoo/hash-example/imt"
@@ -15,10 +16,18 @@ func TestIMT(t *testing.T) {
 var _ = Describe("Hast testing", func() {
 	input1 := []byte{12}
 	output1 := []byte{24, 108, 90, 204, 81, 189, 102, 126}
+	outputhex1 := "186c5acc51bd667e"
 
 	hash1, err := imt.Generate(input1)
 	It("result be hashed", func() {
 		Expect(err).NotTo(HaveOccurred())
 		Expect(hash1).Should(Equal(output1))
+		Expect(hex.EncodeToString(hash1[:])).Should(Equal(outputhex1))
+	})
+
+	var input2 []byte
+	_, err2 := imt.Generate(input2)
+	It("result be hashed", func() {
+		Expect(err2).ToNot(BeNil())
 	})
 })
